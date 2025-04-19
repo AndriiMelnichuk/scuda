f5c706ee98778970ae1651ae9d792c9302b273d8
AndriiMelnichuk <andrii.melnichuk@nure.ua>
2025-04-19 14:46:14 +0300

    fix: Tensor grad x if args empty

--- src/main/scala/tensor/Tensor.scala
+++ src/main/scala/tensor/Tensor.scala
@@ -233,9 +233,6 @@ case class Tensor(val origin: GeneralFunction, val hasVar: Boolean):
 		}, hasVar)
 
 object Tensor:
-	def apply(data: Iterable[Float], shape: Seq[Int], isGrad: Boolean = false)(using  device: String = "cpu"): Tensor = 
-		apply(Storage(data, shape), isGrad)
-	
 	def apply(data: Storage, isGrad: Boolean): Tensor = 
 		lazy val res: Tensor = new Tensor(new GeneralFunction {
 			lazy val args: Seq[Tensor] = Seq()
@@ -243,11 +240,14 @@ object Tensor:
 			def backward(arg: Tensor, chainGrad: Storage): Storage = 
 				if forward.shape != chainGrad.shape then 
 					throw new Exception(s"Gradient x can't be found if forward.shape != chainGrad.shape.\nchainGrad: ${chainGrad.shape}, forward: ${forward.shape}")
-				if arg == res then  Storage.ones(forward) 
-				else                Storage.zeros(arg.storage)
+				if arg == res && isGrad then  Storage.ones(forward) 
+				else                          Storage.zeros(arg.storage)
 		}, isGrad)
 		res
 
+	def apply(data: Iterable[Float], shape: Seq[Int], isGrad: Boolean = false)(using  device: String = "cpu"): Tensor = 
+		apply(Storage(data, shape), isGrad)
+	
 	def fill(shape: Seq[Int], v: =>Float, isGrad: Boolean = false)(using device: String = "cpu") = 
 		apply(Storage.fill(shape, v), isGrad)
 
