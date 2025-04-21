93c946173220a515b37178daf8a24d295f210982
AndriiMelnichuk <andrii.melnichuk@nure.ua>
2025-04-21 17:05:29 +0300

    feat: momentum optimizer

--- src/main/scala/tensor/ai/Optimizer.scala
+++ src/main/scala/tensor/ai/Optimizer.scala
@@ -1,10 +1,20 @@
 package scuda.tensor.ai
 
 import scuda.tensor.storage.Storage
+import scuda.tensor.Tensor
+import scala.compiletime.ops.float
 
 type Weight = Storage
 type Gradient = Storage
 type Optimizer = (Weight, Gradient) => Storage
 
 def SGD(tau: Float): Optimizer =
-    (w0, grad) => -grad * tau + w0
+    (w0, grad) => w0 - (grad * tau)
+
+def momentum(tau: Float, prGrad: Map[Tensor, Storage], mCoef: Float): Optimizer =
+    require(mCoef >= 0 && mCoef < 1, s"momentum optimizer: mCoef must be in [0,1), mCoef: $mCoef")
+    val prGrads = prGrad.map{ (t, s) => (t.storage, s)}
+    (w0, grad) => {
+        val ac = prGrads.getOrElse(w0, Storage.zeros(w0))
+        w0 - (ac * mCoef + grad * tau)
+    }
