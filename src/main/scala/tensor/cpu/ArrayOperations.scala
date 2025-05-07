77a634d7490f47f5eed25eb846977ba1ded35829
AndriiMelnichuk <andrii.melnichuk@nure.ua>
2025-05-07 13:02:49 +0300

    feat (ArrayStorage): broadcasting for element by element operations,  index, slice selection

--- src/main/scala/tensor/cpu/ArrayOperations.scala
+++ src/main/scala/tensor/cpu/ArrayOperations.scala
@@ -4,73 +4,101 @@ import scala.collection.parallel.CollectionConverters._
 import scuda.tensor.storage.Storage
 
 def relu(x: ArrayStorage) = 
-  ArrayStorage(x.storage.map(v => if (v > 0) v else 0f), x.shape)
+	ArrayStorage(x.storage.map(v => if (v > 0) v else 0f), x.shape)
 
 def reluGrad(x: ArrayStorage, cg: ArrayStorage) =
-  val res = ArrayStorage(x.storage.map(x => if x > 0 then 1f else 0f), x.shape)
-  res * cg
+	val res = ArrayStorage(x.storage.map(x => if x > 0 then 1f else 0f), x.shape)
+	res * cg
 
 def sigmoid(x: ArrayStorage): ArrayStorage =
-  val res = x.storage.par.map(x => 1f / (1f + math.exp(-x).toFloat))
-  ArrayStorage(res.toArray, x.shape)
+	val res = x.storage.par.map(x => 1f / (1f + math.exp(-x).toFloat))
+	ArrayStorage(res.toArray, x.shape)
 
 def tanh(x: ArrayStorage): ArrayStorage =
-  val res = x.storage.par.map(x => math.tanh(x).toFloat)
-  ArrayStorage(res.toArray, x.shape)
+	val res = x.storage.par.map(x => math.tanh(x).toFloat)
+	ArrayStorage(res.toArray, x.shape)
 
 /**
-  * 
-  *
-  * @param pr - M x N
-  * @param target N x 1
-  * @return
-  */
+	* 
+	*
+	* @param pr - M x N
+	* @param target N x 1
+	* @return
+	*/
 def crossEntropyLoss(pr: ArrayStorage, target: ArrayStorage): ArrayStorage =
-  val m = pr.shape(0)
-  val n = pr.shape(1)
-  val intTarget = target.storage map (x => x.toInt)
-  val res = (0 until m).par
-    .map(x => x * n + intTarget(x))
-    .map(x => -math.log(pr.storage(x)).toFloat)
-    .toArray
-  new ArrayStorage(res, target.shape)
+	val m = pr.shape(0)
+	val n = pr.shape(1)
+	val intTarget = target.storage map (x => x.toInt)
+	val res = (0 until m).par
+		.map(x => x * n + intTarget(x))
+		.map(x => -math.log(pr.storage(x)).toFloat)
+		.toArray
+	new ArrayStorage(res, target.shape)
 
 def crossEntropyLossGrad(pr: ArrayStorage, target: ArrayStorage, chainGrad: ArrayStorage): ArrayStorage =
-  val m = pr.shape(0)
-  val n = pr.shape(1)
-  val iTarget = target.storage.map(x => x.toInt)
-  val res = for
-    i <- 0 until m
-    j <- 0 until n
-  yield if target.storage(i) == j then -chainGrad.storage(i) / pr.storage(i * n + j) else 0
-  new ArrayStorage(res.toArray, pr.shape)
+	val m = pr.shape(0)
+	val n = pr.shape(1)
+	val iTarget = target.storage.map(x => x.toInt)
+	val res = for
+		i <- 0 until m
+		j <- 0 until n
+	yield if target.storage(i) == j then -chainGrad.storage(i) / pr.storage(i * n + j) else 0
+	new ArrayStorage(res.toArray, pr.shape)
 
 def stableSoftmax(x: ArrayStorage): ArrayStorage = 
-  val m = x shape 0
-  val n = x shape 1
+	val m = x shape 0
+	val n = x shape 1
 
-  val st = x.storage
-  val max = (st grouped n map (x => x.max)).toList
-  val ste = (0 until m * n).par.map(i => math.exp(st(i) - max(i / n)).toFloat).toArray
-  val sums = (ste grouped n).map(_.sum).toArray
+	val st = x.storage
+	val max = (st grouped n map (x => x.max)).toList
+	val ste = (0 until m * n).par.map(i => math.exp(st(i) - max(i / n)).toFloat).toArray
+	val sums = (ste grouped n).map(_.sum).toArray
 
-  val res = (0 until m * n ).par.map(i => ste(i) / sums(i / n)).toArray
-  ArrayStorage(res, x.shape)
-  
+	val res = (0 until m * n ).par.map(i => ste(i) / sums(i / n)).toArray
+	ArrayStorage(res, x.shape)
+	
 def stableSoftmaxGrad(sm: ArrayStorage, cg: ArrayStorage): ArrayStorage =
-  val m = sm shape 0
-  val n = sm shape 1
-  def dldx(i: Int, j: Int) =
-    (0 until n).par map ( k =>
-      if k == j then
-        sm.storage(i * n + j) * (1 - sm.storage(i * n + j)) * cg.storage(i * n + k)
-      else
-        -sm.storage(i * n + j) * sm.storage(i * n + k) * cg.storage(i * n + k)
-    ) reduce (_ + _)
-  
-  val res = for
-    i <- (0 until m).par
-    j <- 0 until n
-  yield dldx(i, j)
+	val m = sm shape 0
+	val n = sm shape 1
+	def dldx(i: Int, j: Int) =
+		(0 until n).par map ( k =>
+			if k == j then
+				sm.storage(i * n + j) * (1 - sm.storage(i * n + j)) * cg.storage(i * n + k)
+			else
+				-sm.storage(i * n + j) * sm.storage(i * n + k) * cg.storage(i * n + k)
+		) reduce (_ + _)
+	
+	val res = for
+		i <- (0 until m).par
+		j <- 0 until n
+	yield dldx(i, j)
 
-  ArrayStorage(res.toArray, sm.shape)
+	ArrayStorage(res.toArray, sm.shape)
+
+def broadcasting(x: ArrayStorage, shape: Seq[Int]): ArrayStorage =
+	def shapeCheck(rs: Seq[Int], es: Seq[Int]): Unit = 
+		if es.length > rs.length then
+			val res = Seq.fill(es.length - rs.length)(1) concat rs
+			shapeCheck(res, es)
+		else if es.length == rs.length then
+			(es zip rs).foreach{ (e, r) =>
+				if r > e then
+					throw new Exception(s"broadcasting: real shape > expected shape. rs: $rs, es: $es")
+				else if r != 1 && r != e then 
+					throw new Exception(s"broadcasting: expected shape: $e, real shape: $r")
+
+			}
+		else 
+			throw new Exception(s"broadcasting: real shape len > expected shape len. rs: $rs, es: $es")
+	def recursiveBroadcasting(a: Array[Float], n: Int): ArrayStorage =
+		if n == -1                     then ArrayStorage(a, shape)
+		else if x.shape(n) == shape(n) then recursiveBroadcasting(a, n - 1)
+		else
+			val elems2copy = shape.drop(n + 1).product
+			val copyCount = shape(n)
+			val res = a.grouped(elems2copy).flatMap { group =>
+				Array.fill(copyCount)(group).flatten
+			}.toArray
+			recursiveBroadcasting(res, n - 1)
+	shapeCheck(x.shape, shape)
+	recursiveBroadcasting(x.storage, shape.length - 1)
--- src/main/scala/tensor/cpu/ArrayStorage.scala
+++ src/main/scala/tensor/cpu/ArrayStorage.scala
@@ -5,6 +5,7 @@ import scuda.tensor.cuda.CudaStorage
 import scuda.tensor.utils.{ beautifulArrayprint, host2device }
 import scala.collection.parallel.CollectionConverters._
 import scala.util.Random
+import scala.annotation.tailrec
 
 class ArrayStorage(
 	val storage: Array[Float], 
@@ -17,10 +18,13 @@ class ArrayStorage(
 	require(storage.length == shape.product, "ArrayStorage: size of the data must correspond to shape")
 	require(shape.map(_ > 0).reduce(_ && _), "ArrayStorage: shape must not contain dim < 0")
 
-	def elementByElementOperation(operation: (Float, Float) => Float)(other: Storage): Storage = 
-		if this.shape != other.shape then throw new Exception("Operation cannot be performed if shape of Tensors isn't equal.")
+	def elementByElementOperation(operation: (Float, Float) => Float)(other: Storage): ArrayStorage = 
 		other match
-			case other: ArrayStorage => ArrayStorage(storage.zip(other.storage).map(operation(_,_)), shape)
+			case other: ArrayStorage => 
+				if this.shape != other.shape then 
+					if shape.product > other.shape.product then elementByElementOperation(operation)(broadcasting(other, shape))
+					else broadcasting(this, other.shape).elementByElementOperation(operation)(other)
+				else ArrayStorage(storage.zip(other.storage).map(operation(_,_)), shape)
 			case _ => throw new Exception("Operation cannot be performed if devise isn't same.")
 	
 	override def toString(): String = beautifulArrayprint(storage, shape)
@@ -29,7 +33,7 @@ class ArrayStorage(
 	def -(other: Storage) = elementByElementOperation(_ - _)(other)
 	def *(other: Storage) = elementByElementOperation(_ * _)(other)
 	def /(other: Storage) = elementByElementOperation(_ / _)(other)
-	def **(other: Storage): Storage = 
+	def **(other: Storage): ArrayStorage = 
 		if this.shape.length != 2 || other.shape.length != 2 then throw new Exception("Operation cannot be performed if shape of Tensors isn't equal 2.")
 		if this.shape(1) != other.shape(0) then throw new Exception("Operation cannot be performed if Tensor A.shape(1) != Tensor B.shape(0)")
 
@@ -49,25 +53,27 @@ class ArrayStorage(
 				ArrayStorage(nStorage.toArray, Seq(m, n))
 			case _  => throw new Exception("Operation cannot be performed if devise isn't same.")
 
-	def +(alpha: Float) = new ArrayStorage(storage.map(_ + alpha), shape)
-	def -(alpha: Float) = new ArrayStorage(storage.map(_ - alpha), shape)
-	def *(alpha: Float): Storage = new ArrayStorage(storage.map(_ * alpha), shape)
-	def /(alpha: Float): Storage = new ArrayStorage(storage.map(_ / alpha), shape)    
-	def pow(n: Float) =
+	def +(alpha: Float): ArrayStorage = new ArrayStorage(storage.map(_ + alpha), shape)
+	def -(alpha: Float): ArrayStorage = new ArrayStorage(storage.map(_ - alpha), shape)
+	def *(alpha: Float): ArrayStorage = new ArrayStorage(storage.map(_ * alpha), shape)
+	def /(alpha: Float): ArrayStorage = new ArrayStorage(storage.map(_ / alpha), shape)    
+	def pow(n: Float): ArrayStorage =
 		new ArrayStorage(storage.par.map(x => math.pow(x.toDouble,n).toFloat).toArray, shape)
-	def unary_- = new ArrayStorage(storage.map(-_), shape)
+	def unary_- : ArrayStorage = new ArrayStorage(storage.map(-_), shape)
 
 	// device change
-	def toCpu: ArrayStorage = new ArrayStorage(storage.clone(), shape)
+	def toCpu: ArrayStorage = this
 	def toCuda: CudaStorage = new CudaStorage(host2device(storage, shape.product), shape)
 
 	// reduce
-	def sum: Storage = ArrayStorage(Array(storage.par.reduce(_ + _)), Seq(1))
+	def sum: ArrayStorage = ArrayStorage(Array(storage.par.reduce(_ + _)), Seq(1))
+	def sum(axis: Int = 0) = 
+		???
 	def item: Float = 
 		if shape == Seq(1) then storage(0)
 		else throw new Exception("It is impossible to take an element from the storage if shape != Seq(1)")
 
-	def T: Storage = 
+	def T: ArrayStorage = 
 		if shape.length != 2 then throw new Exception("not 2d Tensor cant be transponed")
 		val newStorage =
 		for
@@ -77,21 +83,46 @@ class ArrayStorage(
 
 		ArrayStorage(newStorage.toArray, shape.reverse)
 
-	def reshape(seq: Iterable[Int]): Storage = 
+	def reshape(seq: Iterable[Int]): ArrayStorage = 
 		ArrayStorage(storage, seq.toSeq)
 
-	// TODO index selection
-	def apply(args: Int | Iterable[Int]*) =
-		def helper(index: Int, level: Int) = ???
-		args.foreach{
-			case i: Int => println(s"Int: $i")
-			case it: Iterable[Int] => println(s"Iterable ${it.toList}")
-			case _ => println("who knows")
-			
-		}
-		???
+	def apply(args: Seq[Int | Iterable[Int]]): ArrayStorage =
+		def selectByAxis(x: ArrayStorage, axis: Int = 0, axisN: Int = 0): ArrayStorage =
+			if axisN == -1 then return x
+			if axisN > x.shape(axis) - 1 then throw Exception("invalid axis number")
 
-	def cat(st: Storage, dim: Int = 0) = 
+			val nShape = x.shape.patch(axis,Nil,1)
+			val s      = x.shape.reverse.scanLeft(1)(_*_).init.reverse(axis)
+			val a      = x.shape(axis)
+
+			val resIdx = for 
+				n <- 0 until nShape.product / s
+				r <- 0 until s
+			yield s * (a * n + axisN) + r
+			val res = resIdx.map(x.storage)
+
+			ArrayStorage(res, nShape)
+
+		def selectByAxes(x: ArrayStorage, axis: Int, axisN: Seq[Int]): ArrayStorage =
+			if axisN.length > x.shape(axis) then throw new Exception("")
+			if axisN.map(_ > x.shape(axis) - 1).reduce(_ || _) then throw Exception("")
+			
+			axisN.map{
+				selectByAxis(x, axis, _).reshape(x.shape.updated(axis, 1))
+			}
+			.reduce((x, y) => x.cat(y, axis))
+
+		@tailrec
+		def helper(st: ArrayStorage, args: Seq[Int | Iterable[Int]], ax: Int = 0): ArrayStorage = 
+			args match
+				case Nil                         => st
+				case (i: Int) +: tail            => helper(selectByAxis(st, ax, i), tail, ax + 1)
+				case (it: Iterable[Int]) +: tail => helper(selectByAxes(st, ax, it.toSeq), tail, ax + 1)
+		if args.length > shape.length then throw new Exception("Too many axes")
+		helper(this, args)
+		
+
+	def cat(st: Storage, dim: Int = 0): ArrayStorage = 
 		st match
 			case st: ArrayStorage => 	
 				if dim < 0 || dim >= shape.length then
@@ -119,11 +150,11 @@ class ArrayStorage(
 object ArrayStorage:
 	def apply(data: Iterable[Float], shape: Seq[Int]): ArrayStorage = 
 		new ArrayStorage(data.toArray, shape)
-	def fill(shape: Seq[Int], value: =>Float): Storage = 
+	def fill(shape: Seq[Int], value: =>Float): ArrayStorage = 
 		val newStorage = Array.fill(shape.product)(value)
 		new ArrayStorage(newStorage, shape)
-	def ones(shape: Seq[Int]): Storage = fill(shape, 1)
-	def zeros(shape: Seq[Int]): Storage = fill(shape, 0)
-	def rand(shape: Seq[Int]): Storage = 
+	def ones(shape: Seq[Int]): ArrayStorage = fill(shape, 1)
+	def zeros(shape: Seq[Int]): ArrayStorage = fill(shape, 0)
+	def rand(shape: Seq[Int]): ArrayStorage = 
 		val r = Random()
 		ArrayStorage.fill(shape, r.nextFloat())
