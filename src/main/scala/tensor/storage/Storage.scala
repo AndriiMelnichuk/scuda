32d58740be723e334f748b24eb3d5dcbbeba4494
AndriiMelnichuk <andrii.melnichuk@nure.ua>
2025-05-07 18:57:29 +0300

    feat (Storage): select by axis and axes

--- src/main/scala/tensor/storage/Storage.scala
+++ src/main/scala/tensor/storage/Storage.scala
@@ -31,6 +31,7 @@ trait Storage:
 	def reshape(seq: Int*): Storage = 
 		reshape(seq.toIterable)
 	def reshape(seq: Iterable[Int]): Storage
+	def apply(args: Seq[Int | Iterable[Int]]): Storage
 	def flatten(from: Int = 0, to: Int = shape.length) =
 		require(from >= 0)
 		require(from < to)
